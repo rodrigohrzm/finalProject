de6ffe4f1efa79eaf4caa7be05026ef64f77a24a
rodrigohrzm <rherranzm@gmail.com>
2023-03-17 17:42:04 +0100

    basic html structure

---
+++ reactcapstone/src/Footer.js
@@ -0,0 +1,32 @@
+function Footer() {
+    return (
+      <footer>
+        <section><img src={require("./assets/restauranfood.jpg")} width="160" /></section>
+        <section>
+          <p class="footerTitle">NAVIGATION</p>
+          <ul>
+          <li><a href={"https://littlelemonrestaurant.com"}>Home</a></li>
+          <li><a href={"https://littlelemonrestaurant.com/about"}>About</a></li>
+          <li><a href={"https://littlelemonrestaurant.com/menu"}>Menu</a></li>
+          <li><a href={"https://littlelemonrestaurant.com/booking"}>Reservations</a></li>
+          <li><a href={"https://littlelemonrestaurant.com/orders"}>Order Online</a></li>
+          <li><a href={"https://littlelemonrestaurant.com/login"}>Login</a></li>
+        </ul></section>
+        <section>
+          <p class="footerTitle">CONTACT</p>
+          <ul>
+          <li><a href={"https://maps.google.com/littlelemon"}>Address</a></li>
+          <li><a href={"tel:123456789456123"}>Telephone</a></li>
+          <li><a href={"mailto:hello@littlelemonrestaurant.com"}>Email</a></li>
+        </ul></section>
+        <section>
+          <p class="footerTitle">SOCIAL MEDIA</p>
+          <ul>
+          <li><a href={"https://facebook.com/littlelemon"}>Facebook</a></li>
+          <li><a href={"https://linkedin.com/littlelemon"}>LinkedIn</a></li>
+          <li><a href={"https://telegram.com/littlelemon"}>Telegram</a></li>
+        </ul></section>
+      </footer>
+    )};
+
+  export default Footer;
---
+++ reactcapstone/src/Nav.js
@@ -0,0 +1,20 @@
+import {ReactComponent as Logo} from './assets/Logo.svg';
+
+function Nav() {
+    return (
+      <>
+      <Logo />
+      <nav>
+        <ul>
+          <li><a href={"https://littlelemonrestaurant.com"}>Home</a></li>
+          <li><a href={"https://littlelemonrestaurant.com/about"}>About</a></li>
+          <li><a href={"https://littlelemonrestaurant.com/menu"}>Menu</a></li>
+          <li><a href={"https://littlelemonrestaurant.com/booking"}>Reservations</a></li>
+          <li><a href={"https://littlelemonrestaurant.com/orders"}>Order Online</a></li>
+          <li><a href={"https://littlelemonrestaurant.com/login"}>Login</a></li>
+        </ul>
+      </nav>
+      </>
+    )};
+// https://create-react-app.dev/docs/adding-images-fonts-and-files/
+  export default Nav;
