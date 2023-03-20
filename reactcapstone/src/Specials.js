398211f8e996e2f29147cfc29a64886e91ac0eae
rodrigohrzm <rherranzm@gmail.com>
2023-03-20 17:31:34 +0100

    dishes topbar

--- reactcapstone/src/Specials.js
+++ reactcapstone/src/Specials.js
@@ -1,9 +1,16 @@
+import { Button } from "@chakra-ui/react";
+
 function Specials() {
     return (
-      <div class="titleBar">
-        <h1>Book a Table</h1>
-        <img src={require("./assets/restaurant.jpg")} />
+      <section class="specialDishes">
+      <div class="topArea">
+        <h2>This week's Specials!</h2>
+        <button>Online Menu</button>
       </div>
+      <div class="cardsGrid">
+        <div class="Card"></div>
+      </div>
+      </section>
     )};
 
   export default Specials;
