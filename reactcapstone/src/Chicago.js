181299c9c3675f86c5faa1967fbfb3942ec36752
rodrigohrzm <rherranzm@gmail.com>
2023-03-20 08:20:37 +0100

    chicagodesc

--- reactcapstone/src/Chicago.js
+++ reactcapstone/src/Chicago.js
@@ -1,8 +1,12 @@
 function Chicago() {
     return (
-      <div class="titleBar">
-        <h1>Book a Table</h1>
-        <img src={require("./assets/restaurant.jpg")} />
+      <div class="chicagoDesc">
+        <div>
+        <h2>Little Lemon</h2>
+        <h3>Chicago</h3>
+        <p>Little Lemon is a charming Chicago restaurant known for its fresh and flavorful cuisine. The menu features a variety of dishes inspired by Asian and Middle Eastern flavors, with a focus on using locally-sourced ingredients. The restaurant's cozy atmosphere and friendly service make it a popular spot for both locals and tourists alike.</p>
+        </div>
+        <img src={require("./assets/Mario and Adrian A.jpg")} />
       </div>
     )};
 
