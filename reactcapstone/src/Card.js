7374aa6e01567b803686734e5a9876667a64dde6
rodrigohrzm <rherranzm@gmail.com>
2023-03-21 12:40:46 +0100

    cards v2

--- reactcapstone/src/Card.js
+++ reactcapstone/src/Card.js
@@ -2,18 +2,6 @@ import {ReactComponent as Basket} from './assets/Basket.svg';
 import { Link } from "react-router-dom"
 import React from "react"
 
-// const Card = ({ title, description, imageSrc }) => {
-//   return (
-//   <Box roundedTop="20" roundedBottom="20" backgroundColor="#ffffff" >
-//   <Image borderRadius="20px" src={imageSrc} alt="cardimage" />
-//   <Stack px={4} py={4}>
-//   <Heading py={2} px={2} fontFamily="helvetica" color="#2a2a2a" as='h4' size='md'>{title}</Heading>
-//   <Text px={2} fontSize="20px" color="#64748b" fontFamily="Calibri">{description}</Text>;
-//   <HStack py={2} px={2} color="black"><a href="https://www.youtube.com/watch?v=s0akG-V_Y30">See more</a> <FontAwesomeIcon icon={faArrowRight} size="1x" /></HStack>
-//   </Stack>
-//   </Box>
-// )};
-
 const Card = ({ title, price, description, imageSrc }) => {
     return(
         <div class="cardContainer">
@@ -22,7 +10,7 @@ const Card = ({ title, price, description, imageSrc }) => {
             <text>{price}</text>
             <text>{description}</text>
             <button><Link to={"/"}>
-                Online Menu <Basket />
+                Order a delivery <Basket class="ico" />
             </Link></button>
         </div>
     );
