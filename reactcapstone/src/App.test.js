5f410da94340578e454a1b8df91350d2e1e51c57
rodrigohrzm <rherranzm@gmail.com>
2023-03-23 17:24:45 +0100

    unit testing

--- reactcapstone/src/App.test.js
+++ reactcapstone/src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import BookingForm from './BookingForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('Renders the Number of Guests label', () => {
+  render(<BookingForm />);
+  const labelElement = screen.getByText("Number of guests");
+  expect(labelElement).toBeInTheDocument();
+})
