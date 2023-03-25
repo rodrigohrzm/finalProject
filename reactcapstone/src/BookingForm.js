84d26b60efca8fdf87f128ebb076ad0f3d373f18
rodrigohrzm <rherranzm@gmail.com>
2023-03-25 21:01:28 +0100

    logic and functionality completed only test and styling remains

--- reactcapstone/src/BookingForm.js
+++ reactcapstone/src/BookingForm.js
@@ -12,18 +12,24 @@ function BookingForm({availabletimes, settimes}) {
       margin: "100px auto",
       gap: "20px",
    };
-
    let submitstyle = {
       width: "30vh",
       marginLeft: "auto",
       marginRight: "auto"
    };
+   // input:required:invalid,
+   // input:focus:invalid {
+   //   /* insert your own styles for invalid form input */}
+   // selected fields highlighted, return button from bookingpage previous page
+   //    <Link to=".." relative="path">
 
    const [ date , setDate ] = useState ("");
    const [ time , setTime ] = useState ("");
    const [ guests , setGuests ] = useState ("");
    const [ occasion , setOccasion ] = useState ("");
    const [ location , setLocation ] = useState ("");
+   const [ fullname , setFullname ] = useState ("");
+   const [ email , setEmail ] = useState ("");
 
    function auxchild(event) {
       const newDate = event.target.value;
@@ -34,7 +40,7 @@ function BookingForm({availabletimes, settimes}) {
 
    const handleSubmit = (e) => {
       e.preventDefault();
-      let formData = [date, time, guests, occasion, location];
+      let formData = [date, time, guests, occasion, location, fullname, email];
       if (submitAPI(formData) === true) {navigate("/confirmation");
           } else { return null}
       }
@@ -43,7 +49,7 @@ function BookingForm({availabletimes, settimes}) {
         <form style={tempstyle} onSubmit={handleSubmit}>
          <fieldset>
             <label htmlFor="res-date">Choose date</label>
-               <input type="date" id="res-date" value={date} /* onChange={(e) => {setDate(e.target.value); settimes(date)}}*/ onChange={auxchild}/>
+               <input type="date" id="res-date" value={date} onChange={auxchild}/>
             <label htmlFor="res-time">Choose time</label>
                <select id="res-time" value={time} onChange={(e => setTime(e.target.value))}>
                   {availabletimes.map((slot) => <option key={slot} value={slot}>{slot}</option>)}
@@ -64,6 +70,12 @@ function BookingForm({availabletimes, settimes}) {
                   <label htmlFor="outdoors">Outdoors</label>
                <input type="radio" id="indoors" name="indoors" value="indoors" checked={location === "indoors"} onChange={(e => setLocation(e.target.value))}/>
                   <label htmlFor="indoors">Indoors</label>
+         </fieldset>
+         <fieldset>
+            <label htmlFor="fullname">Your name</label>
+               <input required minlength="3" type="text" id="fullname" value={fullname} onChange={(e => setFullname(e.target.value))}/>
+            <label htmlFor="email">Contact email</label>
+               <input required type="email" id="email" value={email} onChange={(e => setEmail(e.target.value))}/>
          </fieldset>
         <button disabled={!occasion || !location || !guests || !time || !date} style={submitstyle} type="submit">Submit reservation</button>
         </form>
